744597139f25d6710895880ac2fbcf966657b348
Pubudusandeepa <39061732+Pubudusandeepa@users.noreply.github.com>
2019-12-20 23:16:57 +0530

    first commit

---
+++ src/component/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from '../strems/StreamCreate';
+import StreamDelete from '../strems/StreamDelete';
+import StreamEdit from '../strems/StreamEdit';
+import  StreamList from '../strems/StreamList';
+import StreamShow from '../strems/StreamShow';
+import Header from './Header';
+
+
+const App = () =>{
+
+    return (
+        <div>
+            <BrowserRouter>
+            <div>
+                <Header/>
+         <Route path="/" exact component={StreamList} />
+         <Route path="/streams/new" component={StreamCreate} />
+         <Route path="/streams/edit" component={StreamEdit} />
+         <Route path="/streams/delete" component={StreamDelete} />
+         <Route path="/streams/show" component={StreamShow} />
+
+            </div>
+       
+        </BrowserRouter> 
+        </div>
+    
+    );
+
+};
+
+
+export default App;
---
+++ src/component/GoogleAuth.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends React.Component{
+    state = {isSignedIn: null };
+   
+
+  componentDidMount(){
+      window.gapi.load('client:auth2', () =>{
+          window.gapi.client.init({
+              clientId: '202493013885-pljotbbcdv2usagifh1moo41ns13vuse.apps.googleusercontent.com',
+              scope: 'email'
+          }).then(() =>{
+              this.auth = window.gapi.auth2.getAuthInstance();
+              this.setState({ isSignedIn : this.auth.isSignedIn.get()})
+              this.auth.isSignedIn.listen(this.onAuthChange);
+          })
+      });
+  }
+
+    onAuthChange = isSignedIn =>{
+        if(isSignedIn){
+            this.props.signIn();
+        }else{
+            this.props.signOut();
+        }
+    }
+
+    onSigiInClick =() =>{
+        this.auth.signIn();
+      
+    }
+
+    onSignOutClick =() =>{
+       this.auth.signOut();
+     
+
+    }
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return <div>I dont know if we are signed in</div>
+        }else if(this.state.isSignedIn){
+            return(
+              <button onClick={this.onSignOutClick} className="ui red google button">
+              <i className="google icon" />
+              Sign Out
+              </button>
+
+            );
+        }else{
+            return(
+                <button onClick={this.onSigiInClick} className="ui red google button">
+                <i className="google icon" />
+                Sign in with Google
+                </button>
+            );
+        }
+    }
+
+    render(){
+        return <div>{this.renderAuthButton()}</div>;
+    }
+}
+
+export default connect(null, { signIn,signOut})(GoogleAuth);
