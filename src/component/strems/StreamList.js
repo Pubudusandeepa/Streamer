f83dd927503d5e3c531c5f2fe5cd3c0344f7e7f5
Pubudusandeepa <ppsandeepa12@gmail.com>
2019-12-29 00:18:01 +0530

    addUserId

--- src/component/strems/StreamList.js
+++ src/component/strems/StreamList.js
@@ -12,7 +12,7 @@ class StreamList extends React.Component {
                return (
               <div className="item" key={stream.id}>
                    <i className ="large middle aligned icon camera" />
-               <div className= "conttent">
+               <div className= "content">
                     {stream.title} 
                <div className="description">{stream.description}</div>
                </div>
