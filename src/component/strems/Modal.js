aca0b257017c6aa0c0ee5f660668702120bc2f7a
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-01-18 21:58:01 +0530

    create delete model

--- src/component/strems/Modal.js
+++ src/component/strems/Modal.js
@@ -6,7 +6,16 @@ const Modal =  props =>{
     return ReactDOM.createPortal(
         <div className= "ui dimmer modals visible active">
             <div className="ui standard modal visible active">
-                 kdmwowjfwjnfj efnwefkln
+               <div className="header">
+                  Delete Stream
+               </div>
+               <div className="content">
+                  Are you sure you want to delete this stream?
+               </div>
+               <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cncel</button>
+               </div>
             </div>
 
         </div>,
